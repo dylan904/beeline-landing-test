e799f7ccdb4803816a25582fd5ae0d81670adf6b
Dylan <tazmaxey@yahoo.com>
2021-06-22 19:15:14 -0400

    joy2

--- deploy.sh
+++ deploy.sh
@@ -21,5 +21,6 @@ git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f https://github.com/dylan904/beeline-landing-test.git master:gh-pages
 
 cd -
